b6c4f6a8fbc298f1975dc25784a105011ae9677a
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-04-20 14:48:03 +0600

    added routing

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "../pages/Home";
 import MainLayout from "../layouts/MainLayout";
+import About from "../pages/About";
+import PhoneDetails from "../pages/PhoneDetails";
+import Favorites from "../pages/Favorites";
+import Errorpage from "../pages/Errorpage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: MainLayout,
+    errorElement: <Errorpage />,
     children: [
+      {
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/about",
+        Component: About,
+      },
+      {
+        path: "/favorites",
+        Component: Favorites,
+      },
+      {
+        path: "/phoneDetails",
+        Component: PhoneDetails,
+      },
       {
         path: "/",
         Component: Home,
